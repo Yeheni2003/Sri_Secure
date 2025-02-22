7169d0b81bfcb1667246359627af72fbbf1cb260
Yeheni2003 <164992015+Yeheni2003@users.noreply.github.com>
2025-02-22 11:15:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Contact form submission alert
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+  });
+  
